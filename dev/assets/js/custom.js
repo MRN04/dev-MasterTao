6500fe4fd3bb17ffa99bb25bf881bb49833dd83d
Maksym Peretiatko <maksym.peretiatko@lnu.edu.ua>
2024-06-27 22:29:41 +0300

    made burger menu

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,15 @@
-  
+const burger = document.querySelector(".burger")
+const BurgerMenu = document.querySelector(".burger-menu")
+const header = document.querySelector("header")
+
+function ShowBurgerMenu() {
+    BurgerMenu.classList.toggle("show")
+    burger.classList.toggle("cross")
+
+}
+function removeClass() {
+    BurgerMenu.classList.remove("show")
+}
+
+burger.addEventListener("click", ShowBurgerMenu)
+/*header.addEventListener("click", removeClass)*/
