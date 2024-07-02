8c569a1b9f3b81e6cc49c76d6370dd641f7bcd25
Maksym Peretiatko <maksym.peretiatko@lnu.edu.ua>
2024-07-02 14:39:34 +0300

    made first screen: desctop and mobile versions

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -13,3 +13,21 @@ function removeClass() {
 
 burger.addEventListener("click", ShowBurgerMenu)
 /*header.addEventListener("click", removeClass)*/
+
+const swiper = new Swiper('.swiper', {
+    speed: 500,
+    spaceBetween: 30,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
